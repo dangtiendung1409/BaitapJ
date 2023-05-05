1a78e17d8408c12665c9e04473ff14e347eed882
Tien Dung <dungdtth2209011@fpt.edu.vn>
2023-05-05 11:11:46 +0700

    btvn

--- src/demo4/Cart.java
+++ src/demo4/Cart.java
@@ -8,41 +8,52 @@ public class Cart {
     private double grandTotal;
     private ArrayList<Product> productList;
     private String city;
-
+    // Constructor ko tham số, tạo ô nhớ cho mảng
     public Cart() {
         productList = new ArrayList<Product>();
         city = "";
     }
 
     public Cart(String city) {
-        productList = new ArrayList<Product>();
         this.city = city;
+        productList = new ArrayList<Product>();
     }
 
+    // Thêm sản phẩm vào giỏ hàng
     public void addProduct(Product product) {
         productList.add(product);
-        grandTotal += product.getPrice();
-        if (city.equals("Hanoi") || city.equals("Ho Chi Minh City")) {
-            grandTotal *= 1.01;
-        } else {
-            grandTotal *= 1.02;
-        }
+        updateGrandTotal(product);
     }
 
+    // Xoá sản phẩm khỏi giỏ hàng
     public void removeProduct(Product product) {
         productList.remove(product);
-        grandTotal -= product.getPrice();
+        updateGrandTotal(product);
+    }
+
+    // Cập nhật tổng tiền
+    private void updateGrandTotal(Product product) {
+        grandTotal = 0.0;
+        for (Product p : productList) {
+            grandTotal += (p.getPrice() * p.getQty());
+            if (city.equals("Hanoi") || city.equals("Ho Chi Minh City")) {
+                grandTotal += p.getShippingFee() * p.getQty() * 1.01;
+            } else {
+                grandTotal += p.getShippingFee() * p.getQty() * 1.02;
+            }
+        }
         if (city.equals("Hanoi") || city.equals("Ho Chi Minh City")) {
-            grandTotal *= 1.01;
+            grandTotal += product.getShippingFee() * product.getQty() * 1.01;
         } else {
-            grandTotal *= 1.02;
+            grandTotal += product.getShippingFee() * product.getQty() * 1.02;
         }
     }
 
+    // Tính tổng tiền
     public void calculateTotal() {
-        grandTotal = 0.0; // Initialize grandTotal to 0.0 before calculating the total
+        grandTotal = 0.0;
         for (Product product : productList) {
-            grandTotal += (product.getPrice() * product.getQty());
+            grandTotal += product.getPrice() * product.getQty();
         }
         if (city.equals("Hanoi") || city.equals("Ho Chi Minh City")) {
             grandTotal *= 1.01;
@@ -50,7 +61,6 @@ public class Cart {
             grandTotal *= 1.02;
         }
     }
-
 
     public void setCity(String city) {
         this.city = city;
--- src/demo4/Product.java
+++ src/demo4/Product.java
@@ -5,17 +5,15 @@ public class Product {
     private String productName;
     private int qty;
     private double price;
+    private double shippingFee;
 
-    public int getQty() {
-        return qty;
-    }
     public Product(int id, String productName, int qty, double price) {
         this.id = id;
         this.productName = productName;
         this.qty = qty;
         this.price = price;
     }
-
+   // kiểm tra hàng
     public void checkQty() {
         if (qty > 0) {
             System.out.println("Còn hàng");
@@ -23,12 +21,21 @@ public class Product {
             System.out.println("Hết hàng");
         }
     }
-
+    public int getQty() {
+        return qty;
+    }
     public double getPrice() {
         return price;
     }
-
     public void setPrice(double price) {
         this.price = price;
+    }
+
+    public double getShippingFee() {
+        return shippingFee;
+    }
+
+    public void setShippingFee(double shippingFee) {
+        this.shippingFee = shippingFee;
     }
 }
